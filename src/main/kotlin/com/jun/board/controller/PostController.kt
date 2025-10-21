388fb6802b9bc21b8a7b8a2802aaadb43284f5f2
kimD0ngjun <chickentasty0112@gmail.com>
2025-10-21 21:38:43 +0900

    refactor: 게시글 삭제 서비스 파라미터 추가

--- src/main/kotlin/com/jun/board/controller/PostController.kt
+++ src/main/kotlin/com/jun/board/controller/PostController.kt
@@ -1,6 +1,9 @@
 package com.jun.board.controller
 
 import com.jun.board.controller.request.PostRequest
+import com.jun.board.controller.request.toDto
+import com.jun.board.controller.response.PostResponse
+import com.jun.board.domain.dto.toResponse
 import com.jun.board.service.PostService
 import org.springframework.web.bind.annotation.*
 
@@ -13,25 +16,25 @@ class PostController(
     fun createPost(
         @RequestBody postRequest: PostRequest,
         username: String // 나중에 @AuthenticationPrincipal userDetails: UserDetailsImpl 로 교체
-    ) {}
+    ): PostResponse = postService.createPost(postRequest.toDto(username)).toResponse()
 
     @GetMapping("/{id}")
     fun getPost(
         @PathVariable("id") id: Long
-    ) {}
+    ): PostResponse = postService.getPost(id).toResponse()
 
     @GetMapping
-    fun getAllPosts() {}
+    fun getAllPosts(): List<PostResponse> = postService.getAllPosts().map { it.toResponse() }
 
     @PatchMapping
     fun updatePost(
         @RequestBody postRequest: PostRequest,
         username: String // 나중에 @AuthenticationPrincipal userDetails: UserDetailsImpl 로 교체
-    ) {}
+    ): PostResponse = postService.updatePost(postRequest.toDto(username)).toResponse()
 
     @DeleteMapping("/{id}")
     fun deletePost(
         @PathVariable("id") id: Long,
         username: String // 나중에 @AuthenticationPrincipal userDetails: UserDetailsImpl 로 교체
-    ) {}
+    ): Unit = postService.deletePost(id, username)
 }
--- src/main/kotlin/com/jun/board/service/PostService.kt
+++ src/main/kotlin/com/jun/board/service/PostService.kt
@@ -2,6 +2,7 @@ package com.jun.board.service
 
 import com.jun.board.domain.dto.PostDTO
 import com.jun.board.domain.dto.toEntity
+import com.jun.board.domain.entity.Post
 import com.jun.board.domain.entity.toDTO
 import com.jun.board.repository.PostRepository
 import org.springframework.stereotype.Service
@@ -42,7 +43,11 @@ class PostService(
         return entity.toDTO()
     }
 
-    fun deletePost(id: Long): Unit {
-        if (postRepository.existsById(id)) postRepository.deleteById(id)
+    fun deletePost(id: Long, username: String): Unit {
+        postRepository.findById(id)
+            .orElseThrow { IllegalArgumentException("해당 게시글을 찾을 수 없음") }
+            .takeIf { it.username == username } // 조건에 맞으면 객체 그대로 반환 아니면 null 반환 조건부 필터링 함수
+            ?.let { postRepository.delete(it) } // 블록 내 null 안전 처리 + 값 반환/변환 후 반환
+            ?: throw java.lang.IllegalArgumentException("작성자만 삭제 가능")
     }
 }
