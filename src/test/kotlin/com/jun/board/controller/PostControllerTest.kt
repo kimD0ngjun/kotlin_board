e121999f20695b19f84f99e133479b38174d63c0
kimD0ngjun <chickentasty0112@gmail.com>
2025-11-08 19:26:43 +0900

    test: kotest와 컨트롤러 테스트에 대한 내 생각 끄적끄적

--- src/test/kotlin/com/jun/board/controller/PostControllerTest.kt
+++ src/test/kotlin/com/jun/board/controller/PostControllerTest.kt
@@ -3,24 +3,20 @@ package com.jun.board.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.jun.board.controller.request.PostRequest
 import com.jun.board.service.PostService
-import io.kotest.core.spec.style.FunSpec
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-//@WebMvcTest(PostController::class)
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
@@ -50,6 +46,16 @@ class PostControllerTest @Autowired constructor(
             .andReturn()
     }
 }
+
+/**
+ * kotest는 리플렉션으로 Spec()을 생성
+ * 그래서 기본 생성자를 무조건 요구함
+ * 근데 컨트롤러 테스트는 내 코드 말고도 mvc나 오브젝트 매퍼 등 부수적인 것들 의존성 주입이 요구됨
+ * lateinit을 하자니 초기화 정합성 예외가 터지고, 그렇다고 의존성 주입을 하자니 기본생성자가 없어서 안 되고
+ *
+ * 가능한 방법이 없으려나? 컨트롤러 테스트 같은 경우는 kotest가 부적합한 경우려나?
+ */
+
 //@ExtendWith(SpringExtension::class)
 //internal class PostControllerTest: FunSpec({
 //    lateinit var mockMvc: MockMvc
