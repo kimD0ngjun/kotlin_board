c783af3a54d0b357608df162a03bc5d3e3628977
kimD0ngjun <chickentasty0112@gmail.com>
2025-11-08 18:37:54 +0900

    chore: 코틀린 버전 업

--- src/test/kotlin/com/jun/board/controller/PostControllerTest.kt
+++ src/test/kotlin/com/jun/board/controller/PostControllerTest.kt
@@ -1,15 +1,53 @@
 package com.jun.board.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.jun.board.controller.request.PostRequest
 import com.jun.board.service.PostService
-import org.junit.jupiter.api.Assertions.*
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import io.mockk.mockk
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@WebMvcTest(PostController::class)
-internal class PostControllerTest {
+//@WebMvcTest(PostController::class)
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class PostControllerTest: DescribeSpec() {
+    val postService: PostService = mockk<PostService>()
     @Autowired
-    private lateinit var mockMvc: MockMvc
+    lateinit var mockMvc: MockMvc
     @Autowired
-    private lateinit var postService: PostService
+    lateinit var objectMapper: ObjectMapper
+
+    init {
+        beforeSpec {
+            // mock bean 스프링 컨텍스트에서 교체 수동 주입
+            // mockk(relaxed = true) : 아무 설정 없어도 호출 시 예외 x, 기본값 자동 반환
+            every { postService.createPost(any()) } returns mockk(relaxed = true) // 아무 설정 없어도 호출 시 예외 x, 기본값 자동 반환
+        }
+
+        describe("게시글 컨트롤러") {
+            context("POST /post 요청시") {
+                it("200 응답 반환") {
+                    val request = PostRequest("제목", "내용")
+
+                    val result = mockMvc.perform(
+                        post("/post")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+                    )
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(status().isOk)
+                        .andReturn()
+                }
+            }
+        }
+    }
 }
